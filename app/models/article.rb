0d488b2a0d5f9ab46a6a9c150257ace96323f581
Michel Jamati <michel.jamati@gmail.com>
2017-12-01 12:48:53 +0000

    Added article model, validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
-#  validates :title, presence: true
+  validates :title, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 10}
+
+#errors: @object.errors.any?
+#error_messages: @object.errors.full_messages
+
 end
