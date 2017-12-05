64f729c70a265a3ea63387280f2f091ee962c33a
Michel Jamati <michel.jamati@gmail.com>
2017-12-05 02:10:59 +0000

    Bootstrap mods that were not added; other mods

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,65 @@
+class ArticlesController < ApplicationController
+
+before_action :set_article, except: [:new, :create, :index]
+
+def index
+  @articles = Article.all
+end
+
+def show
+  @article = Article.find(params[:id])
+end
+
+def new
+  @article = Article.new
+end
+
+def update
+  @article.assign_attributes(article_params)
+  if @article.save
+    flash[:notice] = "Article was successfully updated."
+    redirect_to article_path(@article)
+  else
+    flash[:notice] = "Article update failure."
+    render :new
+  end
+end
+
+def edit
+end
+
+def create
+  @article = Article.new(article_params)
+  if @article.save
+    flash[:notice] = "Article was successfully created."
+    redirect_to article_path(@article)
+  else
+    flash[:notice] = "Article creation failure."
+    render :new
+  end
+end
+
+def destroy
+  if @article.destroy
+    flash[:notice] = "Article was successfully deleted."
+    redirect_to articles_path
+  else
+    flash[:notice] = "Article deletion failure."
+  end
+end
+
+protected
+
+def set_article
+  @article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(
+    :title,
+    :description
+  )
+end
+
+end
+
